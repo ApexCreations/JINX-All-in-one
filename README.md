e440533070a15c3341f6e96869cfaf5dc12199c5
Savvy <malcomeqgreen@gmail.com>
2018-02-20 16:46:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jinx All In One
---
+++ src/main/java/org/apexcreations/jinx/Jinx.java
@@ -0,0 +1,17 @@
+package org.apexcreations.jinx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jinx extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
